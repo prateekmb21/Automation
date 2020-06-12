ace32421e6533156dacaceb1457968ea946c140d
Prateek Bhiwapurkar <prateekbhiwapurkar@Prateeks-MacBook-Pro.local>
2020-06-12 11:56:16 +0530

    added code to have wait until and also xpath now declared above

--- features/step_definitions/flipkart.rb
+++ features/step_definitions/flipkart.rb
@@ -18,6 +18,9 @@ require 'selenium-webdriver'
   driver = Selenium::WebDriver.for(:remote,
     :url => "http://prateekbhiwapurk2:5yDG4MWRdXoFx8Fzg2Mi@hub-cloud.browserstack.com/wd/hub",
     :desired_capabilities => caps)
+  wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+
+  resultXpath = "//div[@class='bhgxx2 col-12-12']//div[@class='_3wU53n']"
 
 Given(/^I am on "([^"]*)" homepage$/) do |website|
   driver.navigate.to "http://www.#{website}"
@@ -27,11 +30,11 @@ When(/^I search for "([^"]*)" in search bar and hit enter$/) do |product|
   element = driver.find_element(:name, 'q')
   element.send_keys product
   element.submit
-  sleep 10
+  element = wait.until { driver.find_element(:xpath => resultXpath) }
 end
 
 Then(/^I should see relevant search results$/) do
-  elements = driver.find_elements(:xpath, "//div[@class='bhgxx2 col-12-12']//div[@class='_3wU53n']")
+  elements = driver.find_elements(:xpath, resultXpath)
   puts "Elements array size: #{elements.size}"
   elements.each do |e|
   	puts "Printing element text"
