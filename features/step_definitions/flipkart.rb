5f1379cc3a31c0faa0c74b8c8b86028faab3b1c6
Prateek Bhiwapurkar <prateekbhiwapurkar@Prateeks-MacBook-Pro.local>
2020-06-12 14:08:57 +0530

    used class and removed xpath

--- features/step_definitions/flipkart.rb
+++ features/step_definitions/flipkart.rb
@@ -14,14 +14,10 @@ require 'test-unit'
   	"browserVersion" => "11.0",
   }
 
-
-
   driver = Selenium::WebDriver.for(:remote,
     :url => "http://" + ENV['USERNAME'] + ":" + ENV['PASSWORD'] + "@hub-cloud.browserstack.com/wd/hub",
     :desired_capabilities => caps)
   wait = Selenium::WebDriver::Wait.new(:timeout => 15)
-
-  resultXpath = "//div[@class='bhgxx2 col-12-12']//div[@class='_3wU53n']"
 
 Given(/^I am on "([^"]*)" homepage$/) do |website|
   driver.navigate.to "http://www.#{website}"
@@ -31,14 +27,13 @@ When(/^I search for "([^"]*)" in search bar and hit enter$/) do |product|
   element = driver.find_element(:name, 'q')
   element.send_keys product
   element.submit
-  element = wait.until { driver.find_element(:xpath => resultXpath) }
+  element = wait.until { driver.find_element(:class => "_3wU53n") }
 end
 
 Then(/^I should see relevant search results$/) do
   elements = driver.find_elements(:class, "_3wU53n")
   puts "Elements array size: #{elements.size}"
   elements.each do |e|
-  	puts "Printing element text"
   	puts e.text
   end
   assert_equal elements.size,10
